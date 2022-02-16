0828b33bb5e2b993a3c4ce7b6ba9f300323cf306
monika barmola <barmolamonika@gmail.com>
2022-02-16 09:14:13 +0530

    new repo

---
+++ src/main/java/org/sap/MyMaven/App.java
@@ -0,0 +1,13 @@
+package org.sap.MyMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
